2278e8ee13977c95a547e5fa560d3216b2136dfb
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-04-03 07:32:32 -0500

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> anagram_map;
+        for (string str : strs) {
+            string sorted = str;
+            sort(sorted.begin(),sorted.end());
+            anagram_map[sorted].push_back(str);
+        }
+        vector<vector<string>> res;
+        for (auto [key,value] : anagram_map) {
+            res.push_back(value);
+        }
+        return res;
+    }
+};
