5b7f37c3d27b17d17ff50e243632e1fc4623453a
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-04-07 08:12:23 -0500

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> unique;
+        for (int i=0;i<nums.size();i++) {
+            for (int j=i+1;j<nums.size();j++) {
+                for (int k=j+1; k<nums.size(); k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        vector<int> temp{nums[i], nums[j], nums[k]};
+                        sort(temp.begin(),temp.end());
+                        unique.insert(temp);
+                    }
+                }
+            }
+        }
+
+        vector<vector<int>> res(unique.begin(),unique.end());
+        return res;
+    }
+};
