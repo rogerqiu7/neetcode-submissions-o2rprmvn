e966cc6887e6b252d0053f005116c502414a4cae
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-04-08 09:09:06 -0500

    Add: trapping-rain-water - submission-10

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-10.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+
+        int max_l = height[l];
+        int max_r = height[r];
+        int total_area = 0;
+
+        while (r>l) {
+            if (height[r] >= height[l]) {
+                l++;
+                max_l = max(max_l,height[l]);
+                total_area += (max_l - height[l]);
+            } else {
+                r--;
+                max_r = max(max_r,height[r]);
+                total_area += (max_r - height[r]);
+            }
+        }
+
+        return total_area;
+    }
+};
