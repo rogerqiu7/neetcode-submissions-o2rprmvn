4973caafb2e6a03b2429ca97eaffb1657dceb89a
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-04-10 07:39:13 -0500

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = 0;
+        int sell = 1;
+        int max_profit = 0;
+
+        while (sell < prices.size()) {
+            if (prices[sell] > prices[buy]) {
+                int current = prices[sell] - prices[buy];
+                max_profit = max(max_profit, current);
+            } else {
+                buy = sell;
+            }
+            sell++;
+        }
+        return max_profit;
+    }
+};
