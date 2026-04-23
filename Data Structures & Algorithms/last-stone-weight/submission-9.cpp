78a32d23d989af27fa63e488cd3fa18aff6d1c0b
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-04-23 07:55:29 -0500

    Add: last-stone-weight - submission-9

---
+++ Data Structures & Algorithms/last-stone-weight/submission-9.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        while (stones.size()>1) {
+            sort(stones.begin(), stones.end());
+
+            int a = stones.back();
+            stones.pop_back();
+            int b = stones.back();
+            stones.pop_back();
+
+            stones.push_back(a-b);
+        }
+
+        return stones[0];
+    }
+};
