98ced96c72a6fd1e9dcbb180d7466230b3b27f14
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-04-25 08:14:10 -0500

    Add: last-stone-weight - submission-14

---
+++ Data Structures & Algorithms/last-stone-weight/submission-14.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for (int stone:stones) {
+            maxHeap.push(stone);
+        }
+
+        while (maxHeap.size() > 1){
+            int a = maxHeap.top();
+            maxHeap.pop();
+            int b = maxHeap.top();
+            maxHeap.pop();
+
+            if (a != b) {
+                maxHeap.push(a-b);
+            }
+        }
+
+        maxHeap.push(0);
+        return maxHeap.top();
+    }
+};
