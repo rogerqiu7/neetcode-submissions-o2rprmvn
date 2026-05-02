24c57491d97b034582ec41bf6c29d29e606e2a73
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-05-02 07:40:03 -0500

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.cpp
@@ -0,0 +1,29 @@
+class MyHashSet {
+public:
+
+    unordered_set<int> mySet;
+
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        mySet.insert(key);
+    }
+    
+    void remove(int key) {
+        mySet.erase(key);
+    }
+    
+    bool contains(int key) {
+        return mySet.contains(key);
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
