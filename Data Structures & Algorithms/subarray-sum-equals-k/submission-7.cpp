2c2430508e9671ea38904bc22764f9f15ae0ad5c
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-05-06 08:05:05 -0500

    Add: subarray-sum-equals-k - submission-7

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int res = 0;
+        for (int i = 0; i<nums.size();i++) {
+            int current = 0;
+            for (int j = i; j<nums.size(); j++) {
+                current += nums[j];
+                if (current == k) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
