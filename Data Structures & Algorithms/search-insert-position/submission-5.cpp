8d068c7d4b07675c355af8f4b2b004d0f5557b8a
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-05-17 07:41:38 -0500

    Add: search-insert-position - submission-5

---
+++ Data Structures & Algorithms/search-insert-position/submission-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int count = 0;
+        for (int num : nums) {
+            if (num < target) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
