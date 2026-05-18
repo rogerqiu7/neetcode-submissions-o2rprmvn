312e64e2a3f2092c076ecd1304676055c3f7ef9f
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-05-18 08:43:52 -0500

    Add: sqrtx - submission-14

---
+++ Data Structures & Algorithms/sqrtx/submission-14.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int res = 0;
+        for (long long i = 0; i<=x; i++) {
+            if (i*i <= x) {
+                res = i;
+            } else {
+                break;
+            }
+        }
+
+        return res;
+    }
+};
