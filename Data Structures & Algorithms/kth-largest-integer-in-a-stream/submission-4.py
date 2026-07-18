42601985b9a6e24cd391552ac2686d035b517ee4
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-07-18 09:21:17 -0500

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.py
@@ -0,0 +1,10 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.array = nums
+
+    def add(self, val: int) -> int:
+        self.array.append(val)
+        self.array.sort()
+        return self.array[-self.k]
