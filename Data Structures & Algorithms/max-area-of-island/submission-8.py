bb1b11f45ebcbeda2e81e5c2006bcccd4bef631f
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2026-07-20 07:13:21 -0500

    Add: max-area-of-island - submission-8

---
+++ Data Structures & Algorithms/max-area-of-island/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        res = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        directions = [[1,0], [0,1], [-1,0], [0,-1]]
+        queue = deque()
+
+        def bfs():
+            count = 1
+            while queue:
+                row, col = queue.popleft()
+                for dr, dc in directions:
+                    new_row, new_col = row+dr, col+dc
+                    if (0<=new_row<rows and 0<=new_col<cols and grid[new_row][new_col]==1):
+                        count += 1
+                        grid[new_row][new_col] = 0
+                        queue.append((new_row,new_col))
+            return count
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    queue.append((row,col))
+                    grid[row][col] = 0
+                    current = bfs()
+                    res = max(current, res)
+
+        return res
